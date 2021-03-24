b4bb790afdc637c3517d4f9238d94fd4f1cac772
Micaela Martini <80223363+martinimicaela@users.noreply.github.com>
2021-03-24 20:46:44 -0300

    Add files via upload

---
+++ primerprograma.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+#include <cstdlib>
+
+int main(){
+    //Suma de dos números enteros con C/C++
+    float a, b, r;
+    cout << "Ingrese un número entero: ";
+    cin >> a;
+    cout << "Ingrese otro número entero: ";
+    cin >> b;
+    r = a+b;
+    cout << "El resultado de la suma es: " << r;
+
+
+ system("pause>nul");
+ return 0;
+}
