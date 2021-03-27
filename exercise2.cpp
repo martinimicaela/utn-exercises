6c15b40c24e5ea44fe237e11261a110f79cfe293
Micaela Martini <80223363+martinimicaela@users.noreply.github.com>
2021-03-27 15:00:16 -0300

    Add files via upload

---
+++ exercise2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+#include <cstdlib>
+
+//Ejercicio 3
+
+int main(){
+   const int sueldofijo=15000, premiofijo=2000;
+   //const int premiofijo = 2000;
+   int autosvendidos, sueldototal;
+   cout << "Ingrese la cantidad de autos vendidos: ";
+   cin >> autosvendidos;
+
+   sueldototal = (autosvendidos*premiofijo)+sueldofijo;
+
+   cout << " Su sueldo es: " << sueldototal;
+
+
+ system("pause>nul");
+ return 0;
+}
